d98226f8eee6bb64b723044d07ebda2fab7ee8b2
Spike Strous <Spike.strous@gmail.com>
2018-11-28 13:50:23 +0100

    database change

--- models/User.js
+++ models/User.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 let UserSchema = new mongoose.Schema({
     username: {type: String, unique: true, required: [true, "Can't be blank"]},
-    password: {type: String, required: ["Can't be blank"]}
+    password: {type: String, required: [true, "Can't be blank"]}
 });
 
 const User = mongoose.model('User', UserSchema);
