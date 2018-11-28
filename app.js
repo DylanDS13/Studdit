8130cfcf1e1cb4da6e53a803ea0290ca1be5289e
Spike Strous <Spike.strous@gmail.com>
2018-11-28 13:33:34 +0100

    Added user post

--- app.js
+++ app.js
@@ -1,16 +1,23 @@
 const createError = require('http-errors');
 const express = require('express');
 const path = require('path');
-const cookieParser = require('cookie-parser');
 const logger = require('morgan');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
 
 const app = express();
 
-app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(require('morgan')('dev'));
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+app.use(express.static(__dirname + '/public'));
+
+mongoose.connect('mongodb://Admin:Admin123@ds119374.mlab.com:19374/studdit_db');
+mongoose.set('debug', true);
+
+const UserController = require('./routes/user');
+app.use('/users', UserController);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
@@ -28,4 +35,6 @@ app.use(function(err, req, res, next) {
   res.render('error');
 });
 
-module.exports = app;
+const server = app.listen( process.env.PORT || 3000, function(){
+    console.log('Listening on port ' + server.address().port);
+});
--- routes/user.js
+++ routes/user.js
@@ -1,2 +1,21 @@
-const router = require('express').Router();
-const mongoose = require('mongoose');
+const express = require('express');
+const router = express.Router();
+const bodyParser = require('body-parser');
+
+router.use(bodyParser.urlencoded({ extended: true }));
+router.use(bodyParser.json());
+
+const User = require('../models/User');
+
+router.post('/', function (req, res){
+    User.create({
+        username: req.body.username,
+        password: req.body.password
+        },
+        function (err, user ) {
+            if (err) return res.status(500).send("There was a problem adding the information to the database.");
+            res.status(200).send(user);
+        })
+});
+
+module.exports = router;
