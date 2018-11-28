9be0bb936737b242eec55fc24d35ba145860b318
Spike Strous <Spike.strous@gmail.com>
2018-11-28 15:14:56 +0100

    Added user delete functionality

--- routes/user.js
+++ routes/user.js
@@ -23,13 +23,25 @@ router.put('/', function (req, res){
         {username: req.body.username,
         password: req.body.oldPassword},
         {$set:{password: req.body.newPassword}},
+        {new: true},
         function (err, doc){
-            console.log(err);
             if (err) return res.status(500).send("Something wrong when updating data!");
 
             res.status(200).send(doc);
         }
     )
 });
+
+router.delete('/', function (req, res){
+    User.findOneAndDelete(
+        {username: req.body.username,
+        password: req.body.password},
+        function (err, doc){
+            if (err) return res.status(500).send("Something wrong when deleting data!");
+
+            res.status(200).send(doc);
+        }
+    )
+})
 
 module.exports = router;
