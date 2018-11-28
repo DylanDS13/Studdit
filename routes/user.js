3bb5537e5eb60aa66e5560dfab7502957187ad94
Spike Strous <Spike.strous@gmail.com>
2018-11-28 15:37:51 +0100

    Changed update and delete to give 401 for wrong password

--- routes/user.js
+++ routes/user.js
@@ -19,29 +19,34 @@ router.post('/', function (req, res){
 });
 
 router.put('/', function (req, res){
-    User.findOneAndUpdate(
-        {username: req.body.username,
-        password: req.body.oldPassword},
-        {$set:{password: req.body.newPassword}},
-        {new: true},
-        function (err, doc){
-            if (err) return res.status(500).send("Something wrong when updating data!");
+    User.findOne({username: req.body.username})
+        .then((user) => {
+            if(user.password === req.body.oldPassword){
+                user.update({password: req.body.newPassword}, function (err, raw){
+                    if (err) return res.status(500).send("Something wrong when deleting data!");
 
-            res.status(200).send(doc);
-        }
-    )
+                    res.status(200).send(raw);
+                })
+            }
+            else {
+                res.status(401).send("Wrong password");
+            }
+        })
 });
 
 router.delete('/', function (req, res){
-    User.findOneAndDelete(
-        {username: req.body.username,
-        password: req.body.password},
-        function (err, doc){
-            if (err) return res.status(500).send("Something wrong when deleting data!");
+    User.findOne({username: req.body.username})
+        .then((user) => {
+            if(user.password === req.body.password){
+                user.remove(function (err, doc){
+                    if (err) return res.status(500).send("Something wrong when deleting data!");
 
-            res.status(200).send(doc);
-        }
-    )
-})
+                    res.status(200).send(doc);
+                })
+            } else {
+                res.status(401).send("Wrong password");
+            }
+        })
+});
 
 module.exports = router;
